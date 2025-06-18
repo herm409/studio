5ed576ad84bf39a78247023f21be147ccbd49b3a
FBC Brokers <herman@freedombychoice.com>
2025-06-18 01:25:10 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -16,6 +17,11 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+        "https://6000-firebase-studio-1750206045453.cluster-pgviq6mvsncnqxx6kr7pbz65v6.cloudworkstations.dev"
     ],
   },
 };
